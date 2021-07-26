523e43ed8dfa85ada3a4eacc72e26db933f7839f
yuka-ma <yuka-ma@NA21037.nature.local>
2021-07-26 11:04:51 +0900

    Employeeモデル、EmployeeViewモデル作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -26,6 +26,9 @@ public enum AttributeConst {
     //管理者フラグ
     ROLE_ADMIN(1), ROLE_GENERAL(0),
 
+    //削除フラグ
+    DEL_FLAG_TRUE(1), DEL_FLAG_FALSE(0),
+
     //日報管理
     REPORT("report"), REPORTS("reports"), REP_COUNT("reports_count"), REP_ID("id"), REP_DATE("report_date"), REP_TITLE(
             "title"), REP_CONTENT("content");
--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -48,17 +48,18 @@ public interface JpaConst {
     String JPQL_PARM_EMPLOYEE = "employee"; //従業員
 
     //NamedQueryのnameとquery
-    //全ての従業員をid降順に取得する
+    //全ての従業員情報をid降順に取得する
     String Q_EMP_GET_ALL = ENTITY_EMP + ".getAll"; //name
     String Q_EMP_GET_ALL_DEF = "SELECT e FROM Employee AS e ORDER BY e.id DESC"; //query
-    //全ての従業員の件数を取得する
+    //全ての従業員情報の件数を取得する
     String Q_EMP_COUNT = ENTITY_EMP + ".count";
     String Q_EMP_COUNT_DEF = "SELECT COUNT(e) FROM Employee AS e";
-    //社員番号とハッシュ化済パスワードを条件に未削除の従業員を取得する
+    //社員番号とハッシュ化(ハッシュ関数を用いた文字列)済パスワードを条件に未削除の従業員を取得する
+    //従業員がログインするときに、社員番号とパスワードが正しいかをチェック
     String Q_EMP_GET_BY_CODE_AND_PASS = ENTITY_EMP + ".getByCodeAndPass";
-    String Q_EMP_GET_BY_CODE_AND_PASS_DEF = "SELECT e FROM Employee AS e WHERE w.deleteFlag = 0 AND e.code = :"
+    String Q_EMP_GET_BY_CODE_AND_PASS_DEF = "SELECT e FROM Employee AS e WHERE e.deleteFlag = 0 AND e.code = :"
             + JPQL_PARM_CODE + " AND e.password = :" + JPQL_PARM_PASSWORD;
-    //指定した社員番号を保持する従業員の件数を取得する
+    //指定した社員番号がすでにDBに保持されているかを調べる
     String Q_EMP_COUNT_RESISTERED_BY_CODE = ENTITY_EMP + ".countRegisteredByCode";
     String Q_EMP_COUNT_RESISTERED_BY_CODE_DEF = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
     //全ての日報をidの降順に取得する
