63c7c5d2da195a76ba07d8619d3b136467811aab
yuka-ma <yuka-ma@NA21037.nature.local>
2021-07-26 17:43:43 +0900

    従業員indexアクションの作成

--- src/main/java/actions/ActionBase.java
+++ src/main/java/actions/ActionBase.java
@@ -56,7 +56,7 @@ public abstract class ActionBase {
         } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException | NullPointerException e) {
 
-            //commandの値が不正で実行できない場合エラー画面を呼び出し
+            //commandの値が不正で実行できない場合、エラー画面を呼び出し
             forward(ForwardConst.FW_ERR_UNKNOWN);
         }
     }
